7e7cffcdae2c922997c9f46e129efbc09b634b0d
artem <amelnychenko06@gmail.com>
2022-07-17 17:49:54 -0400

    Combined components together

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import { useState } from 'react';
 import './App.css';
 
+import Board from './components/Board';
+import Data from './data';
+
 function App() {
+
+  const [chosenChar, setChoseChar] = useState({ x: 0, y: 0, char: '' })
+
+  const playerChoise = (arg1, arg2, arg3) => {
+    setChoseChar({
+      x: arg1,
+      y: arg2,
+      char: arg3
+    })
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h2>Photo Tagging App</h2>
-      </header>
+      <h2>Photo Tagging App</h2>
+      <Board sendChoise={playerChoise} />
+      <Data playerChoise={chosenChar} />
     </div>
   );
 }
