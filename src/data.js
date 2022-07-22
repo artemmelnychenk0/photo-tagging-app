14c5657246be47b8f74cf446d774cc6e7172986a
artem <amelnychenko06@gmail.com>
2022-07-22 11:03:36 -0400

    Added logic for checking coordinates

--- src/data.js
+++ src/data.js
@@ -1,22 +1,19 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react"
 import { db } from "./firebase-config";
-import { collection, getDocs, query, where } from 'firebase/firestore'
+import { collection, getDocs, query, where, } from 'firebase/firestore'
 
 
-const Data = (playerChoise) => {
-    const chosenCoords = playerChoise.playerChoise
-
-    const updateChoise = () => {
-        setChosen({
-            x: chosenCoords.x,
-            y: chosenCoords.y,
-            char: chosenCoords.char
-        })
-    }
-
+const Data = ({ playerChoise, removeChar, isGameOver }) => {
+    const chosenCoords = playerChoise
     const [chosen, setChosen] = useState({ x: 0, y: 0, char: '' })
     const [users, setUsers] = useState([]);
+    const [foundChars, setFoundChards] = useState([])
+    const [foundOne, setFoundOne] = useState('')
+
+    const [wrongDiv, setWrongDiv] = useState(false)
+    const [firstRequest, setFirstRequest] = useState(true)
+    const [showBox, setShowBox] = useState(false)
 
     const userRef = collection(db, 'coords');
     const q = query(userRef, where('x', '==', chosen.x), where('y', '==', chosen.y), where('char', '==', chosen.char))
@@ -32,19 +29,67 @@ const Data = (playerChoise) => {
     useEffect(() => {
         isPlayerRight();
     }, [users])
-    const getUsers = async () => {
-        const data = await getDocs(q);
-        setUsers(data.docs.map((doc) => ({ ...doc.data() })))
 
+    const updateChoise = () => {
+        setChosen({
+            x: Math.round(chosenCoords.x / 100),
+            y: Math.round(chosenCoords.y / 100),
+            char: chosenCoords.char
+        })
     }
 
+    const getUsers = async () => {
+        const data = await getDocs(q);
+        if (data.docs.length === 0 && firstRequest === false) {
+            wrongSelection()
+        }
+        setUsers(data.docs.map((doc) => ({ ...doc.data() })))
+    }
+
+    setTimeout(() => {
+        setFirstRequest(false)
+    }, 1000)
+
     const isPlayerRight = () => {
-        if (users.length === 0) {
-            console.log('Wrong spot')
-        } else {
-            console.log(`You found ${users[0].char}`)
+        if (users.length !== 0) {
+            setFoundChards(arr => [...arr, users[0].char])
+            setFoundOne(users[0].char)
+            removed();
         }
     }
 
+    const wrongSelection = () => {
+        setWrongDiv(true)
+        setTimeout(() => {
+            setWrongDiv(false)
+        }, 2000);
+    }
+
+    const removed = () => {
+        if (foundChars.length === 3) {
+            isGameOver();
+        }
+        setShowBox(current => !current)
+        removeChar(foundOne)
+    }
+
+    return (
+        <div>
+            {showBox &&
+                (<div className="popup-box">
+                    <div className="box2">
+                        <span className="close-icon" onClick={removed}>OK</span>
+                        You found {users[0].char}
+                    </div>
+                </div>)}
+            {wrongDiv &&
+                (<div className="popup-box2">
+                    <div className="box3">
+                        Your choise is wrong
+                    </div>
+                </div>)
+            }
+        </div>
+    )
 }
 export default Data
