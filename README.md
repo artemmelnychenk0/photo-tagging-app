cbdc26be9fa8bf16b239ca27fe46f1040282e5ac
artem <amelnychenko06@gmail.com>
2022-07-22 11:02:47 -0400

    Finished Project

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-## Photo Tagging App
+# Game & Seek (Photo Tagging App - Where's Waldo?)
+
+## Summary:
+<p>The goal of the project is to create a 'Wheres Waldo' type of game that incorporates a backend. Players select a level and must find 3 hidden characters. The characters coordinates are used to identify their location in the level, selecting the correct coordinates (along with the character) will mark a character as found. Firebase is used as a backend to store the game data & highscores for each level. This project as made with the React JS Library, CSS, and Firebase.</p>
+
+## Technologies:
+  <ul>
+  <li>ReactJS</li>
+  <li>CSS</li>
+  <li>Firebase</li>
+ </ul>
+
+## Features:
+
+- Use of firebase-firestore for a live back end database
+- Explitive usernames submitted to the leaderboard are censored
+- Responsive Design
+
+## Screenshots
+<img src="src/assets/screenshots/shot-1.png" height="300px">&nbsp;&nbsp;&nbsp;<img src="src/assets/screenshots/shot-2.png" height="300px">
--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import { useState } from 'react';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Board from './components/Board';
+import Header from './components/Header';
+import ScoreBoard from './components/Scoreboard';
+import Stopwatch from './components/Stopwatch';
 import Data from './data';
+import { formatTime } from './utils/formatTime';
+import Welcome from './components/Welcome';
 
 function App() {
 
   const [chosenChar, setChoseChar] = useState({ x: 0, y: 0, char: '' })
+
+  const [timeRecord, setTimeRecord] = useState(0)
+  const [showWatch, setShowWatch] = useState(false)
+  const [showBoard, setShowBoard] = useState(false)
+  const [startGame, setStartGame] = useState(true)
+
+  const [timer, setTimer] = useState(0)
+
+  const [options, setOptions] = useState([
+    { value: 'Kratos', label: 'Kratos' },
+    { value: 'Nathan-Drake', label: 'Nathan Drake' },
+    { value: 'Arthur-Morgan', label: 'Artur Morgan' },
+  ])
 
   const playerChoise = (arg1, arg2, arg3) => {
     setChoseChar({
@@ -15,15 +33,44 @@ function App() {
       char: arg3
     })
   }
+  const removedChar = (arg1) => {
+    setOptions(options.filter(a => a.value !== arg1))
+  }
 
+  const gameOver = () => {
+    setTimeRecord(formatTime(timer))
+    setShowWatch(false)
+    setShowBoard(true)
+  }
+
+  const start = () => {
+    setStartGame(false)
+    setInterval(() => {
+      setTimer(timer => timer + 1)
+    }, 1000)
+    setShowWatch(true)
+  }
 
   return (
     <div className="App">
-      <h2>Photo Tagging App</h2>
-      <Board sendChoise={playerChoise} />
-      <Data playerChoise={chosenChar} />
+      {startGame &&
+        <div>
+          <Welcome startGame={start} />
+        </div>}
+      <Header />
+      {showWatch &&
+        <div className='watch'>
+          <Stopwatch time={formatTime(timer)} />
+        </div>
+      }
+      <Data playerChoise={chosenChar} removeChar={removedChar} isGameOver={gameOver} />
+      <Board sendChoise={playerChoise} options={options} />
+      {showBoard &&
+        <div className='watch'>
+          <ScoreBoard timeScore={timeRecord} />
+        </div>
+      }
     </div>
   );
 }
-
 export default App;
