b174e21cb9b6ea3278c435984cb1f6fb8cbe8b0f
artem <amelnychenko06@gmail.com>
2022-07-22 11:03:52 -0400

    Cleaned the code

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,25 +1,17 @@
 import { useState } from 'react'
-import boardimg from '../assets/board.jpg'
+import boardimg from '../assets/board2.jpg'
 import Select from 'react-select'
 
-const Board = ({ sendChoise }) => {
+const Board = ({ sendChoise, options }) => {
 
     const [showBox, setShowBox] = useState(false)
     const [coords, setCoords] = useState({ x: 0, y: 0 })
 
-    const options = [
-        { value: 'char1', label: 'char1' },
-        { value: 'char2', label: 'char2' },
-        { value: 'char3', label: 'char3' },
-    ]
-
     const show = (e) => {
         setShowBox(current => !current)
-        console.log(e.clientX)
-        console.log(e.clientY)
         setCoords({
-            x: e.clientX,
-            y: e.clientY
+            x: e.pageX,
+            y: e.pageY
         })
     }
 
@@ -29,27 +21,27 @@ const Board = ({ sendChoise }) => {
     }
 
     return (
-        <div className='board' onClick={show}>
-            {showBox && (<div className='game'
-                style={{
-                    position: 'absolute',
-                    left: coords.x,
-                    top: coords.y
-                }}>
-                <div className='box'>
-                </div>
-                <div className='selector'>
-                    <Select
-                        defaultValue={{ value: 'char1', label: 'char1' }}
-                        defaultMenuIsOpen
-                        options={options}
-                        onChange={selection} />
-                </div>
-            </div>)}
-
-            <img src={boardimg} alt='board'></img>
+        <div className='board' onClick={show} >
+            {showBox &&
+                (<div className='game'
+                    style={{
+                        position: 'absolute',
+                        left: coords.x - 27,
+                        top: coords.y - 27
+                    }}>
+                    <div className='box'>
+                    </div>
+                    <div className='selector'>
+                        <Select
+                            defaultMenuIsOpen
+                            options={options}
+                            onChange={selection} />
+                    </div>
+                </div>)}
+            <div className='boardimg'>
+                <img src={boardimg} alt='board'></img>
+            </div>
         </div>
-
     )
 }
 export default Board
